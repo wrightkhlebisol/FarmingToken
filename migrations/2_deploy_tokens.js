dce741ab599074e66b8f0884c4f74df94715522e
Wright <calebogundiya@gmail.com>
2021-03-13 18:38:11 +0100

    Migration: FarmToken with utility token instance

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,7 +1,12 @@
+const CalebToken = artifacts.require("CalebToken");
 const FarmToken = artifacts.require("FarmToken");
 
 module.exports = async function (deployer, network, accounts) {
+    // Deploy CalebToken
+    await deployer.deploy(CalebToken);
+    const calebToken = await CalebToken.deployed();
+
     // Deploy FarmToken
-    await deployer.deploy(FarmToken);
+    await deployer.deploy(FarmToken, calebToken.address);
     const farmToken = await FarmToken.deployed();
 }
